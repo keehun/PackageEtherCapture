0c3014ca0b915b04f5a501e701b777931178f543
Keehun <keehun@users.noreply.github.com>
2021-05-24 12:30:24 -0500

    Update shim.h

--- Sources/PackageEtherCaptureC/include/shim.h
+++ Sources/PackageEtherCaptureC/include/shim.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include "/System/Volumes/Data/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/pcap/pcap.h"
-//#include <pcap/pcap.h>
+//#include "/System/Volumes/Data/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/pcap/pcap.h"
+#include <pcap/pcap.h>
